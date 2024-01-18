81b95214c74485a97fa81d6ef92ab886909bd6a7
Paul McHugh <2775943m@student.gla.ac.uk>
2024-01-18 21:07:26 +0000

    Completed the chapter 3 excercises

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    path('about/', views.about, name = 'about'),
 ]
