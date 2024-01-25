4e1fa7dfee38996c92be3bb52d8d9499712d7dec
Paul McHugh <2775943m@student.gla.ac.uk>
2024-01-25 15:29:15 +0000

    Completed up to 4.4 in TWD

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+
+
+
+MEDIA_ROOT = MEDIA_DIR 
+MEDIA_URL = '/media/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -65,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,5 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 from django.urls import include
 from rango import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
     path('about/', views.about, name = 'about'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
