7f0a91f5fe37a2fb3ccb90be98f5184dec86a87d
Paul McHugh <2775943m@student.gla.ac.uk>
2024-02-08 19:45:27 +0000

    Completed chapter 9 in TWD

--- rango/models.py
+++ rango/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
-    max_length = 128
-    name = models.CharField(max_length=max_length, unique=True)
+    MAX_LENGTH = 128
+    name = models.CharField(max_length=MAX_LENGTH, unique=True)
     views = models.IntegerField(default = 0)
     likes = models.IntegerField(default = 0)
     slug = models.SlugField(unique=True)
@@ -21,11 +22,21 @@ class Category(models.Model):
         return self.name
     
 class Page(models.Model):
-    max_length = 128
+    MAX_LENGTH = 128
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=max_length)
+    title = models.CharField(max_length=MAX_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.title
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(blank = True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
